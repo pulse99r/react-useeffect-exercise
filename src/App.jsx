8ade4ca3c18831f0cea973055fced68d8f71028b
Angie Munoz <angiepmunoz@gmail.com>
2024-09-05 13:08:30 -0400

    instructions

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./Components/NavBar";
+import EmployeeList from "./Components/EmployeeList";
+
+function App () {
+  return (
+    <>
+      <NavBar />
+      <EmployeeList />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Components/EmployeeList.jsx
@@ -0,0 +1,15 @@
+import Employee from "./Employee";
+import "./EmployeeList.css";
+
+export const EmployeeList = () => {
+  return (
+    <main>
+      <h2>All Staff</h2>
+      <section className="employee-list">
+        <Employee />
+      </section>
+    </main>
+  );
+};
+
+export default EmployeeList;
